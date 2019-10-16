ac0e13852aeb34c12a7b04330a09695b37f9693f
BorderKwok <162082908@qq.com>
2019-10-16 17:29:09 +0800

    ver5.0
    ver5.0

---
+++ dist/miniprograms-rm-tools/assets/dummy/edits.ts
@@ -0,0 +1,27 @@
+export const editsConfig = {
+    RMS:[
+        { name:'Johnny Li',checked:false},
+        { name:'John Fung',checked:false},
+        { name:'Margery Wong',checked:false},
+        { name:'Cameron Smith Jones',checked:false},
+        { name:'May Chung',checked:false},
+        { name:'Fannue Chow',checked:false},
+        { name:'Isabella Lam',checked:true},
+        { name:'Henry Chu',checked:false}
+    ],
+    STATUS:[
+        { name:'Follow-up requires',checked:false},
+        { name:'Meeting scheduled',checked:false},
+        { name:'Discussion in progress',checked:false},
+        { name:'On-oarding',checked:true},
+        { name:'Declined by bank',checked:false},
+        { name:'Account opened',checked:false},
+        { name:'Existing customer',checked:false},
+        { name:'Insufficient/unreachable',checked:false}
+    ],
+    PRIORITY:[
+        { name:'High',checked:false},
+        { name:'Medium',checked:true},
+        { name:'Low',checked:false}
+    ]
+}
